a4df97276a6dd250cfdb3994e694c92a92519cd1
Neal Patel <nealajpatel@gmail.com>
2022-06-22 20:44:30 -0400

    Initial terraform for oidc provider

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "github_organization" {
+  description = "GitHub organization."
+  type        = string
+  default     = "nealajpatel"
+}
+
+variable "github_repositories" {
+  description = "List of GitHub repositories."
+  type        = list(string)
+  default = ["k8s"]
+}
+
+variable "github_thumbprint" {
+  description = "GitHub OpenID TLS certificate thumbprint."
+  type        = string
+  default     = "6938fd4d98bab03faadb97b34396831e3780aea1"
+
+}
+
+variable "max_session_duration" {
+  description = "Maximum session duration in seconds."
+  type        = number
+  default     = 3600
+
+  validation {
+    condition     = var.max_session_duration >= 3600 && var.max_session_duration <= 43200
+    error_message = "Maximum session duration must be between 1 and 12 hours."
+  }
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources."
+  type        = object({})
+  default = {
+    project      = "aws-oidc-gov"
+    organization = "nealajpatel"
+  }
+}
